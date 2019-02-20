2cbf44876faa988f86a73242c726b6fd3fe7f32c
Laura Tan <laura.tan108@gmail.com>
2019-02-19 23:56:23 -0800

    Added content

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,24 @@
 $(function (){
+
   $("#paw2paw-btn").click((e)=> {
     e.preventDefault();
     $(".paw2paw-details").slideToggle();
   });
+
+  $("#jungle-btn").click((e)=> {
+    e.preventDefault();
+    $(".jungle-details").slideToggle();
+  });
+
+  $("#chatty-btn").click((e)=> {
+    e.preventDefault();
+    $(".chatty-details").slideToggle();
+  });
+
+  $("#tweeter-btn").click((e)=> {
+    e.preventDefault();
+    $(".tweeter-details").slideToggle();
+  });
+
+
 });
